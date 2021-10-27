7cc5934632673bb8a36b972a265badb6cfdfef66
Rohit Chaudhary <chaudharyrohit264@gmail.com>
2021-10-27 17:30:01 +0545

    adding CrossOrigin annotation to consume the api from frontend

--- src/main/java/io/javaweb/iplfever/controller/TeamController.java
+++ src/main/java/io/javaweb/iplfever/controller/TeamController.java
@@ -4,11 +4,13 @@ import io.javaweb.iplfever.model.Team;
 import io.javaweb.iplfever.repository.MatchRepository;
 import io.javaweb.iplfever.repository.TeamRepository;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
