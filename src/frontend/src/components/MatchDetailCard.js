179dec55f3ac482de7042ff1d13fa2034c17336b
Rohit Chaudhary <chaudharyrohit264@gmail.com>
2021-11-03 02:04:49 +0545

    Removed a header tag in MatchDetailCard

--- src/frontend/src/components/MatchDetailCard.js
+++ src/frontend/src/components/MatchDetailCard.js
@@ -8,7 +8,7 @@ export const MatchDetailCard = ({teamName, match}) => {
   return (
     <div className="MatchDetailCard">
       <h3>Latest Matches</h3>
-      <h4>Match Details</h4>
+      
       <h1> vs <Link to = {otherTeamRoute}>{otherTeam}</Link></h1>
       <h2>{match.date}</h2>
       <h3>at {match.venue}</h3>
