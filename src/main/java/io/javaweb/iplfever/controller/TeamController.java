bb2957348ba7002b3a2594c8b73ea91f83a618d7
Rohit Chaudhary <chaudharyrohit264@gmail.com>
2021-11-03 00:46:18 +0545

    added match API at backend

--- src/main/java/io/javaweb/iplfever/controller/TeamController.java
+++ src/main/java/io/javaweb/iplfever/controller/TeamController.java
@@ -3,10 +3,15 @@ package io.javaweb.iplfever.controller;
 import io.javaweb.iplfever.model.Team;
 import io.javaweb.iplfever.repository.MatchRepository;
 import io.javaweb.iplfever.repository.TeamRepository;
+import io.javaweb.iplfever.model.Match;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +31,14 @@ public class TeamController {
         Team team = this.teamRepository.findByTeamName(teamName);
         team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName, 4));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesforTeam(@PathVariable String teamName, @RequestParam int year){
+        LocalDate startDate = LocalDate.of(year,1,1);
+        LocalDate endDate = LocalDate.of(year+1,1,1);
+
+        return this.matchRepository.getMatchesByTeamBetweenDates(
+            teamName, startDate, endDate);
     }
 }
